822e7096512c7d0b3e525b262360009ebc545621
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-02-02 23:10:27 +0500

    Create Swap Component

--- src/Components/SwapPage.tsx
+++ src/Components/SwapPage.tsx
@@ -4,7 +4,7 @@ import { Heading } from "@chakra-ui/react";
 
 const SwapPage = () => {
     return (
-        <div className="py-12 px-8">
+        <div className="pt-6 md:py-12 px-8">
             <Heading
                 className="mx-auto max-w-3xl text-center my-[4.8rem]"
                 fontWeight="extrabold"
--- src/Components/Swapper.tsx
+++ src/Components/Swapper.tsx
@@ -4,22 +4,29 @@ import { ArrowDownUp, ArrowLeftRight } from "lucide-react";
 
 const Swapper = () => {
     return (
-        <div>
-            <div className="mt-[5.4rem] md:mt-32 px-5 sm:px-16">
-                <div className="flex flex-col lg:flex-row justify-between gap-4 lg:gap-2 py-10 px-4 md:p-10 bg-[#16182D] rounded-2xl">
+        <div className="mt-[5.4rem] md:mt-32 px-5 sm:px-16 pb-[2.5rem]">
+            <div className="py-10 px-4 md:p-10 bg-[#16182D] rounded-2xl">
+                <div className="flex flex-col lg:flex-row justify-between gap-4 lg:gap-2 bg-[#16182D] rounded-2xl my-1 md:my-3 mx-2 md:mx-4">
                     <div className="bg-[#16182D] my-2 lg:my-1">
                         <div className="flex lg:flex-row gap-3 lg:gap-0 flex-col items-center bg-[#16182D]">
                             <input
                                 className="w-[100%] lg:w-60 xl:w-80 bg-[#16182D] bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                placeholder="0.01"
                                 type="number"
                             />
+
                             <select className="w-[100%] lg:w-[5.2rem] bg-[#16182D] bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-2 leading-8 transition-colors duration-200 ease-in-out">
-                                <option className="!w-[100%]">BTC</option>
-                                <option className="!w-[100%]">ETH</option>
+                                <option className="!w-[100%]">
+                                    BTC
+                                </option>
+
+                                <option className="!w-[100%]">
+                                    ETH
+                                </option>
                             </select>
                         </div>
 
-                        <h3 className="bg-[#16182D] text-slate-300 mt-4 md:text-md">
+                        <h3 className="bg-[#16182D] text-white mt-4 md:text-md">
                             You Pay
                         </h3>
                     </div>
@@ -38,18 +45,31 @@ const Swapper = () => {
                         <div className="flex lg:flex-row gap-3 lg:gap-0 flex-col items-center bg-[#16182D]">
                             <input
                                 className="w-[100%] lg:w-60 xl:w-80 bg-[#16182D] bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                placeholder="0.01"
                                 type="number"
                             />
+
                             <select className="w-[100%] lg:w-[5.2rem] bg-[#16182D] bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-2 leading-8 transition-colors duration-200 ease-in-out">
-                                <option className="!w-[100%]">BNB</option>
-                                <option className="!w-[100%]">SOL</option>
+                                <option className="!w-[100%]">
+                                    BNB
+                                </option>
+
+                                <option className="!w-[100%]">
+                                    SOL
+                                </option>
                             </select>
                         </div>
 
-                        <h3 className="bg-[#16182D] text-slate-300 mt-4 md:text-md">
+                        <h3 className="bg-[#16182D] text-white mt-4 md:text-md">
                             You Receive
                         </h3>
                     </div>
+                </div>
+
+                <div className="flex justify-center bg-[#16182D] mt-8 md:mt-4 mb-2">
+                    <Button className="py-5 px-6 lg:py-7 lg:px-10 text-lg lg:text-xl rounded-full cursor-pointer bg-gradient-to-r from-cyan-600 via-blue-800 to-purple-700">
+                        Swap
+                    </Button>
                 </div>
             </div>
         </div>
